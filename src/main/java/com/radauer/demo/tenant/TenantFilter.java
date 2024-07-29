13ac2e6e1d798c4545beeb96702bf0024d357df7
Andreas Radauer <andreas.radauer@porscheinformatik.com>
2024-07-29 15:32:32 +0200

    update spring version

--- src/main/java/com/radauer/demo/tenant/TenantFilter.java
+++ src/main/java/com/radauer/demo/tenant/TenantFilter.java
@@ -2,13 +2,13 @@ package com.radauer.demo.tenant;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 
+import jakarta.servlet.Filter;
 import org.springframework.stereotype.Component;
 
 /**
@@ -18,13 +18,20 @@ import org.springframework.stereotype.Component;
 public class TenantFilter implements Filter
 {
 
-  public static String TENANT_PARAMETER_NAME = "tenant";
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-    throws IOException, ServletException
-  {
-    HttpServletRequest req = (HttpServletRequest) request;
-    TenantHolder.setTenant(req.getParameter(TENANT_PARAMETER_NAME));
-    chain.doFilter(request, response);
-  }
+    public static String TENANT_PARAMETER_NAME = "tenant";
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException
+    {
+        try {
+            HttpServletRequest req = (HttpServletRequest) request;
+            TenantHolder.setTenant(req.getParameter(TENANT_PARAMETER_NAME));
+            chain.doFilter(request, response);
+        }finally
+        {
+            TenantHolder.clean();
+        }
+
+    }
 }
--- src/main/java/com/radauer/demo/tenant/TenantHolder.java
+++ src/main/java/com/radauer/demo/tenant/TenantHolder.java
@@ -1,20 +1,24 @@
 package com.radauer.demo.tenant;
 
 /**
- * Utility Class to hold the current tenant in a ThreadLocal.
- * the value of the ThreadLocal is bound to a request
+ * Utility Class to hold the current tenant in a ThreadLocal. the value of the ThreadLocal is bound to a request
  */
 public class TenantHolder
 {
-  private static final ThreadLocal<String> tenant = new ThreadLocal<>();
+    private static final ThreadLocal<String> tenant = new ThreadLocal<>();
 
-  public static String getTenant()
-  {
-    return tenant.get();
-  }
+    public static String getTenant()
+    {
+        return tenant.get();
+    }
 
-  public static void setTenant(String tenant)
-  {
-    TenantHolder.tenant.set(tenant);
-  }
+    public static void setTenant(String newTenant)
+    {
+        tenant.set(newTenant);
+    }
+
+    public static void clean()
+    {
+        tenant.remove();
+    }
 }
